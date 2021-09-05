ea8c7928fd56ef4b8d9ffbf5f860d1be301e86f8
Mateusz Puchalski <mateusz.puchalski@gmail.com>
2021-09-05 22:09:07 +0200

    Loops method added to BranchesAndLoops

--- c#/BranchesAndLoops/BranchesAndLoops/Program.cs
+++ c#/BranchesAndLoops/BranchesAndLoops/Program.cs
@@ -4,13 +4,13 @@ namespace BranchesAndLoops
 {
     class Program
     {
-        static void Main(string[] args)
+        static void ExploreIf()
         {
             int a = 5;
             int b = 5;
             int c = 4;
 
-            if ((a + b +c > 10) && (a == b))
+            if ((a + b + c > 10) && (a == b))
             {
                 Console.WriteLine("The answer is greater than 10.");
                 Console.WriteLine("And the first number is equal to second");
@@ -31,7 +31,36 @@ namespace BranchesAndLoops
                 Console.WriteLine("The answer is not greater than 10");
                 Console.WriteLine("And the first number is not equal to the second");
             }
+        }
 
+        static void Loops()
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+            Console.WriteLine("\n");
+
+            counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Hello World! The index os {i}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            //ExploreIf();
+            Loops();
 
         }
     }

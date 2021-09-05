412972a3e86584294be6d1f007ce6e2a341c3255
Mateusz Puchalski <mateusz.puchalski@gmail.com>
2021-09-05 21:27:39 +0200

    WorkWithDoubles class added to NumbersInCSharp

--- c#/NumbersInCSharp/NumbersInCSharp/Program.cs
+++ c#/NumbersInCSharp/NumbersInCSharp/Program.cs
@@ -56,15 +56,47 @@ namespace MyApplication
             int what = max + 3;
             Console.WriteLine($"An example of overflow: {what}");
         }
+
+        static void WorkWithDoubles()
+        {
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+            Console.WriteLine(d);
+
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e + f) / g;
+            Console.WriteLine(h);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1 / 3.0;
+            Console.WriteLine(third);
+
+            double i = 7493.8265;
+            double j = 41.145;
+            double k = 6;
+            double l = 75.908;
+            double calc = (i * j + (k - l)) / i;
+            Console.WriteLine(calc);
+        }
         static void Main(string[] args)
         {
-            WorkWithIntegers();
-            Console.WriteLine("\n");
+            //WorkWithIntegers();
+            //Console.WriteLine("\n");
 
-            OrderPrecedence();
-            Console.WriteLine("\n");
+            //OrderPrecedence();
+            //Console.WriteLine("\n");
 
-            TestLimits();
+            //TestLimits();
+            //Console.WriteLine("\n");
+
+            WorkWithDoubles();
 
 
         }

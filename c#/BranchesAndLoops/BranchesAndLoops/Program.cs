9616108e151c29fb4ddd237858d160f3e74c8c9a
Mateusz Puchalski <mateusz.puchalski@gmail.com>
2021-09-05 22:00:47 +0200

    ExploreIf method added to BranchesAndLoops

--- c#/BranchesAndLoops/BranchesAndLoops/Program.cs
+++ c#/BranchesAndLoops/BranchesAndLoops/Program.cs
@@ -7,9 +7,32 @@ namespace BranchesAndLoops
         static void Main(string[] args)
         {
             int a = 5;
-            int b = 6;
-            if (a + b > 10)
+            int b = 5;
+            int c = 4;
+
+            if ((a + b +c > 10) && (a == b))
+            {
                 Console.WriteLine("The answer is greater than 10.");
+                Console.WriteLine("And the first number is equal to second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+
+            if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+            }
+
+
         }
     }
 }
